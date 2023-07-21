8a8ada38ab2bd5b3ae864240126774d8a30e5b24
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-07-21 07:02:10 +0200

    Add providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Space_Mono } from "next/font/google";
 import "./globals.css";
 
-import { Providers } from "@/providers";
+import { Providers } from "@/components/Providers";
 
 const spaceMono = Space_Mono({ subsets: ["latin"], weight: ["400", "700"] });
 
